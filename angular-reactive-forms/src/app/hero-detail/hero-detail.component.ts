85d7299bd2b4e103bb98caa30408b4992972ad45
Hao Han <hao1.han@bmo.com>
2018-02-16 11:00:51 -0500

    - add form group test

--- angular-reactive-forms/src/app/hero-detail/hero-detail.component.ts
+++ angular-reactive-forms/src/app/hero-detail/hero-detail.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, FormBuilder } from '@angular/forms';
+import { FormControl, FormGroup, FormBuilder, Validators } from '@angular/forms';
+
+import { states } from '../data-model';
 
 @Component({
   selector: 'app-hero-detail',
@@ -9,6 +11,7 @@ import { FormControl, FormGroup, FormBuilder } from '@angular/forms';
 
 export class HeroDetailComponent {
   heroForm: FormGroup; // <--- heroForm is of type FormGroup
+  states = states;
 
   constructor(private fb: FormBuilder) { // <--- inject FormBuilder
     this.createForm();
@@ -16,12 +19,34 @@ export class HeroDetailComponent {
 
   createForm() {
     this.heroForm = this.fb.group({
-      nameInFormGroup: 'angular test', // <--- the FormControl called "name"
+      // nameInFormGroup: 'angular test', // <--- the FormControl called "name"
+      nameInFormGroup: ['', Validators.required], 
+      street: '',
+      city: '',
+      zip: '',
+      power: '',
+      sidekick: ''
     });
   }
 }
 
 /*
+export class HeroDetailComponent {
+  heroForm: FormGroup; // <--- heroForm is of type FormGroup
+
+  constructor(private fb: FormBuilder) { // <--- inject FormBuilder
+    this.createForm();
+  }
+
+  createForm() {
+    this.heroForm = this.fb.group({
+      // nameInFormGroup: 'angular test', // <--- the FormControl called "name"
+      nameInFormGroup: ['', Validators.required], 
+      statusInFormGroup: ['true', Validators.required], 
+    });
+  }
+}
+
 export class HeroDetailComponent implements OnInit {
 
   heroForm = new FormGroup ({
